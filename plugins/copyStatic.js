f43ec41d561169af87ad7003a3f521de65401619
haikelfazzani <haikel.fazzani@hotmail.com>
2023-08-29 10:44:28 +0100

    1.0.8

--- plugins/copyStatic.js
+++ plugins/copyStatic.js
@@ -1,12 +1,11 @@
 import { cp } from 'fs/promises';
-// import { resolve } from 'path';
 
 export function copyStatic(dir = 'static') {
   return {
     name: 'copy-files',
     async buildEnd() {
       await cp(dir, 'dist', { recursive: true });
-      console.log('\n===> Files are copied: success\n');
+      console.log(`\n===> Success Copy: ${dir} \n`);
     },
   };
 }
