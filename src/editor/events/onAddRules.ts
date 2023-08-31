48cfca1305dbe2d2f2a205344664ffc9eb4e19cd
haikelfazzani <haikel.fazzani@hotmail.com>
2023-08-31 19:37:26 +0100

    1.0.9

--- src/editor/events/onAddRules.ts
+++ src/editor/events/onAddRules.ts
@@ -5,11 +5,13 @@ export default function onAddRules(e: any) {
   try {
     const ruleType = e.target.elements[0].value;
     const action = e.target.elements[1].value;
-    chrome.runtime.sendMessage({
-      message: action + ':rules',
-      ruleType,
-      rules: JSON.parse(preRules.innerText)
-    });
+    if (window.confirm(`Do you really want to ${action} these rules? `)) {
+      chrome.runtime.sendMessage({
+        message: action + ':rules',
+        ruleType,
+        rules: JSON.parse(preRules.innerText)
+      });
+    }
   } catch (error) {
     snackbar.classList.remove('d-none')
     snackbarCode.innerText = error.message;
--- src/editor/events/onBlockURL.ts
+++ src/editor/events/onBlockURL.ts
@@ -4,8 +4,10 @@ export default function onBlockURL(e: any) {
   e.preventDefault();
   try {
     const url = e.target.elements[0].value.replace(/http(s)?:\/\//g, '');
-    chrome.runtime.sendMessage({ message: 'block:url', url });
-    e.target.reset();
+    if (window.confirm('Do you really want to block this url? ' + url)) {
+      chrome.runtime.sendMessage({ message: 'block:url', url });
+      e.target.reset();
+    }
   } catch (error) {
     snackbar.classList.remove('d-none')
     snackbarCode.innerText = error.message;
