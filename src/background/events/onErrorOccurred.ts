6004e2dfa4993e9ff25090f129942fd65f5700d4
haikel fazzani <haikel.fazzani@hotmail.com>
2024-04-29 17:22:42 +0100

    1.0.15

--- src/background/events/onErrorOccurred.ts
+++ src/background/events/onErrorOccurred.ts
@@ -4,6 +4,8 @@ import WebResponseErrorDetailsStorage from "../storage/WebResponseErrorDetailsSt
 let counter = 0;
 
 export default async function onErrorOccurred(details: WebResponseErrorDetails) {
+  console.log(details.url);
+  
   if (details.error && ['CONNECTION_REFUSED', 'BLOCKED_BY_CLIENT'].includes(details.error.replace('net::ERR_', ''))) {
     chrome.action.setBadgeText({ text: '' + counter++ });
     WebResponseErrorDetailsStorage.save({ ...details, url: details.url.split('?')[0] });
